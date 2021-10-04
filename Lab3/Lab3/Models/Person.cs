3cb76470111d74fa33e3ee27da940731fea56f23
ChickenLeg <ChickenLeg@github.com>
2021-10-04 13:10:49 -0400

    Lab3- YanniLi

---
+++ Lab3/Lab3/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(100)]
+        public String FirstName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public String LastName { get; set; }
+        public int Age { get; set; }
+        public String Email { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Dob { get; set; }
+        public String Password { get; set; }
+        public String Description { get; set; }
+    }
+}
